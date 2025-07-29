ed3b1d9ff63db17d17ef5cae74c28b097518896c
sanchita paul <spaul@microsite.com>
2025-07-29 13:13:56 +0600

    Task 5: added index to employee table

---
+++ database/migrations/2025_07_29_062409_add_indexes_to_employees_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('employees', function (Blueprint $table) {
+            $table->index('start_date');
+            $table->index('team_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('employees', function (Blueprint $table) {
+            $table->dropIndex(['start_date']);
+            $table->dropIndex(['team_id']);
+        });
+    }
+};
