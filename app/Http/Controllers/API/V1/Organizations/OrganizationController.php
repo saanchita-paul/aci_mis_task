15db748dfc3c69485c2bf3f6798cac69dd0e96e7
sanchita paul <spaul@microsite.com>
2025-07-29 21:52:49 +0600

    code refactor organization and added unit and feature test

--- app/Http/Controllers/API/V1/Organizations/OrganizationController.php
+++ app/Http/Controllers/API/V1/Organizations/OrganizationController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreUpdateOrganizationRequest;
 use App\Models\Organization;
 use App\Services\OrganizationService;
-use Illuminate\Http\Request;
 
 class OrganizationController extends Controller
 {
@@ -21,37 +20,26 @@ class OrganizationController extends Controller
     public function index()
     {
         return $this->organizationService->getAll();
-//        return Organization::all();
     }
 
     public function store(StoreUpdateOrganizationRequest $request)
     {
-//        $data = $request->validate([
-//            'name' => 'required|string|max:255',
-//        ]);
-//
-//        return Organization::create($data);
         return $this->organizationService->create($request->validated());
     }
 
     public function show(Organization $organization)
     {
-//        return $organization;
         return $this->organizationService->getById($organization);
     }
 
     public function update(StoreUpdateOrganizationRequest $request, Organization $organization)
     {
-//        $organization->update($request->only('name'));
-//        return $organization;
         return $this->organizationService->update($organization, $request->validated());
 
     }
 
     public function destroy(Organization $organization)
     {
-//        $organization->delete();
-//        return response()->json(['message' => 'Deleted successfully']);
         $this->organizationService->delete($organization);
         return response()->json(['message' => 'Deleted successfully']);
     }
